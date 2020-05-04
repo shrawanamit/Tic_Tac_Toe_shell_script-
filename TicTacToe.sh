d5a03324e5235c6774f6b641658ad16844439385
shrawanamit <amitkumar06111@gmail.com>
2020-05-05 03:09:58 +0530

    added [use case 3] let's toss who play first

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -6,3 +6,17 @@ resetBoard(){
 }
 echo " O for player 1"
 echo " X for player 2"
+
+toss(){
+   tossCheck=$((RANDOM%2))
+         if [ $tossCheck -eq 0 ]
+   then
+                 echo ""
+                 echo "player 1 has won the toss "
+     player=0
+   else
+                 echo "player 2 has won the toss "
+                 player=1
+         fi
+}
+
