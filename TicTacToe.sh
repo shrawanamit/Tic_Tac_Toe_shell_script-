1d9bd450e0259b03239eddd661af4d659a67e56b
shrawanamit <amitkumar06111@gmail.com>
2020-05-05 03:13:36 +0530

    added [use case 4] print the board so player chose the turn

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -13,10 +13,27 @@ toss(){
    then
                  echo ""
                  echo "player 1 has won the toss "
-     player=0
+                 player=0
    else
                  echo "player 2 has won the toss "
                  player=1
          fi
+}
+
+
+printSampleBoard(){
+   echo "===== SAMPLE BOARD TO ENTER VALUE  === "
+   echo " 0 1 2 "
+   echo " 3 4 5 "
+   echo " 6 7 8 "
+}
+
+printBoard(){
+   echo ""
+   echo "===========PLAYER RUNNEING BOARD =========="
+   echo "r\c 0 1 2"
+   echo "0   ${Arr[0]} ${Arr[1]} ${Arr[2]}"
+   echo "1   ${Arr[3]} ${Arr[4]} ${Arr[5]}"
+   echo "2   ${Arr[6]} ${Arr[7]} ${Arr[8]}"
 }
 
